a91a36c6ef7b2ac35dd50df70ece5aabdbf36ea1
Omerkugel12 <omerkugel12@gmail.com>
2024-07-07 17:29:31 +0300

    CRUD-todos

--- src/pages/TaskDeatailsPage.jsx
+++ src/pages/TaskDeatailsPage.jsx
@@ -1,21 +1,22 @@
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useModalContext } from "@/contexts/ModalContext";
-import { useLoggedInUserTasks } from "@/contexts/loggedInUserTasksContext";
 import api from "@/services/api.service";
-import { Pencil, Pin, PinOff, Trash2, X } from "lucide-react";
+import { Pencil, Pin, PinOff, Trash2, Vote, X } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { Navigate, Outlet, useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { Link } from "react-router-dom";
 
-function TaskDeatailsPage() {
+function TaskDetailsPage() {
   const [task, setTask] = useState(null);
   const { taskId } = useParams();
-  const { loggedInUserTasks, setLoggedInUserTasks } = useLoggedInUserTasks();
   const navigate = useNavigate();
   const { modal, setModal } = useModalContext();
-  // console.log(taskId);
+  const [editTodoInput, setEditTodoInput] = useState(null);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [createNewTodoTitle, setCreateNewTodoTitle] = useState("");
+  const [editTaskInputs, setEditTaskInputs] = useState(false);
 
   useEffect(() => {
     async function fetchTask() {
@@ -27,19 +28,37 @@ function TaskDeatailsPage() {
       }
     }
     fetchTask();
-  }, []);
+  }, [taskId]);
 
   if (!task) {
-    return;
+    return null;
+  }
+
+  async function handleEditTask(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const newTitle = formData.get("title");
+    const newDescription = formData.get("description");
+    const newBody = formData.get("body");
+    const data = { newTitle, newDescription, newBody };
+    console.log(data);
+
+    try {
+      const { data: updatedTask } = await api.patch(`task/${taskId}`, {
+        title: newTitle === "" ? task.title : newTitle,
+        description: newDescription === "" ? task.description : newDescription,
+        body: newBody === "" ? task.body : newBody,
+      });
+      setTask(updatedTask);
+      setEditTaskInputs(false);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleDelete() {
     try {
-      const { data: deletdTask } = await api.delete(`/task/${taskId}`);
-
-      setLoggedInUserTasks((prevTasks) => {
-        return prevTasks.filter((task) => task._id !== taskId);
-      });
+      await api.delete(`task/${taskId}`);
       navigate("/tasks");
       setModal("successDelete");
       setTimeout(() => {
@@ -56,36 +75,89 @@ function TaskDeatailsPage() {
 
   async function handlePinnedChange(e, taskId) {
     try {
-      const res = await api.patch(`/task/${taskId}`, {
+      const res = await api.patch(`task/${taskId}`, {
         isPinned: !task.isPinned,
       });
       const updatedTask = res.data;
-      setLoggedInUserTasks((prevTasks) => {
-        return prevTasks.map((task) =>
-          task._id === taskId ? updatedTask : task
-        );
+      setTask((prevTask) => ({ ...prevTask, isPinned: updatedTask.isPinned }));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleTodoChecked(e, todoId) {
+    try {
+      const updatedTodoList = task.todoList.map((todo) =>
+        todo._id === todoId ? { ...todo, isComplete: !todo.isComplete } : todo
+      );
+
+      const res = await api.patch(`task/${task._id}`, {
+        todoList: updatedTodoList,
       });
+
+      const updatedTask = res.data;
       setTask(updatedTask);
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function handleTodoChecked(e, todoId, taskId) {
+  async function handleDeleteTodo(todoId, taskId) {
+    try {
+      const updatedTodoList = task.todoList.filter(
+        (todo) => todo._id !== todoId
+      );
+      const res = await api.patch(`task/${taskId}`, {
+        todoList: updatedTodoList,
+      });
+      const updatedTask = res.data;
+      setTask(updatedTask);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleUpdateTodo(e, todoId, taskId) {
+    e.preventDefault();
+
     try {
       const updatedTodoList = task.todoList.map((todo) =>
-        todo._id === todoId ? { ...todo, isComplete: !todo.isComplete } : todo
+        todo._id === todoId
+          ? {
+              ...todo,
+              isComplete: false,
+              title: newTodoTitle ? newTodoTitle : todo.title,
+            }
+          : todo
       );
 
-      const res = await api.patch(`/task/${taskId}`, {
+      const res = await api.patch(`task/${taskId}`, {
         todoList: updatedTodoList,
       });
 
       const updatedTask = res.data;
-      setLoggedInUserTasks((prevTasks) =>
-        prevTasks.map((task) => (task._id === taskId ? updatedTask : task))
-      );
       setTask(updatedTask);
+      setEditTodoInput(null);
+      setNewTodoTitle("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleCreateTodo(e, taskId) {
+    e.preventDefault();
+    try {
+      const newTodo = { title: createNewTodoTitle, isComplete: false };
+      const updatedTodoList = [...task.todoList, newTodo];
+
+      const res = await api.patch(`task/${taskId}`, {
+        todoList: updatedTodoList,
+      });
+
+      const updatedTask = res.data;
+
+      setTask(updatedTask);
+      setCreateNewTodoTitle("");
     } catch (error) {
       console.log(error);
     }
@@ -94,34 +166,138 @@ function TaskDeatailsPage() {
   return (
     <>
       <div className="fixed top-0 bottom-0 right-0 left-0 bg-slate-700 opacity-80"></div>
-      <div className="fixed top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 z-50 flex flex-col border border-ring p-10 max-w-lg min-w-[32rem] bg-secondary rounded-lg shadow-2xl space-y-4">
+      <div className="fixed top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 z-50 flex flex-col border border-ring p-10 max-w-lg min-w-[32rem] bg-secondary rounded-lg shadow-2xl space-y-4 max-h-[70%] overflow-y-scroll">
         <Link className="fixed top-2 left-2" to={"/tasks"}>
           <X color="#ff0000" />
         </Link>
-        <h1 className="text-2xl">{task.title}</h1>
-        <p>{task.description}</p>
-        <p>{task.body}</p>
-        {task.todoList.length > 0 ? (
-          <ul>
-            {task.todoList.map((todo) => {
-              return (
-                <li key={todo._id}>
-                  <input
-                    type="checkbox"
-                    checked={todo.isComplete}
-                    onChange={(e) => handleTodoChecked(e, todo._id, task._id)}
-                  />
-                  <label className={todo.isComplete ? "line-through" : null}>
-                    {todo.title}
-                  </label>
-                </li>
-              );
-            })}
-          </ul>
-        ) : (
-          <p>No todos yet...</p>
-        )}
-        <div className="flex gap-4 absolute top-4 right-4">
+        <div className="space-y-8 pb-12">
+          <form onSubmit={(e) => handleEditTask(e)}>
+            {!editTaskInputs ? (
+              <h1 className="text-2xl">{task.title}</h1>
+            ) : (
+              <div>
+                <Label>Title</Label>
+                <Input
+                  type="text"
+                  name="title"
+                  placeholder="Enter new task title..."
+                />
+              </div>
+            )}
+            {!editTaskInputs ? (
+              <p>
+                <span className="font-bold">Description:</span>{" "}
+                {task.description}
+              </p>
+            ) : (
+              <div>
+                <Label className="font-bold">Description:</Label>
+                <Input
+                  name="description"
+                  type="text"
+                  placeholder="Enter new task description..."
+                />
+              </div>
+            )}
+            {!editTaskInputs ? (
+              <p>
+                <span className="font-bold">Body:</span> {task.description}
+              </p>
+            ) : (
+              <div>
+                <Label className="font-bold">Body:</Label>
+                <Input
+                  type="text"
+                  name="body"
+                  placeholder="Enter new task body..."
+                />
+              </div>
+            )}
+            {editTaskInputs ? <Button>Apply</Button> : null}
+          </form>
+          <div className="bg-white py-4 rounded-lg flex flex-col px-10 gap-4">
+            {task.todoList.length > 0 ? (
+              <ul className="flex flex-col space-y-4">
+                {task.todoList.map((todo) => {
+                  return (
+                    <li key={todo._id}>
+                      {editTodoInput !== todo._id ? (
+                        <div className="flex justify-between w-100% bg-secondary p-1 rounded-lg px-4">
+                          <div className="flex items-center gap-2">
+                            <input
+                              type="checkbox"
+                              checked={todo.isComplete}
+                              onChange={(e) => handleTodoChecked(e, todo._id)}
+                            />
+                            <label
+                              className={
+                                todo.isComplete ? "line-through" : null
+                              }
+                            >
+                              {todo.title}
+                            </label>
+                          </div>
+                          <div className="flex gap-1">
+                            <button
+                              onClick={() =>
+                                handleDeleteTodo(todo._id, task._id)
+                              }
+                              variant="outlet"
+                              className="text-destructive transition-all hover:scale-110"
+                            >
+                              <Trash2 className="text-destructive " />
+                            </button>
+                            <button
+                              variant="outlet"
+                              className="text-primary transition-all  hover:scale-110"
+                              onClick={() => setEditTodoInput(todo._id)}
+                            >
+                              <Pencil className="text-primary" />
+                            </button>
+                          </div>
+                        </div>
+                      ) : (
+                        <form
+                          onSubmit={(e) =>
+                            handleUpdateTodo(e, todo._id, task._id)
+                          }
+                          className="flex justify-between w-100% bg-secondary p-1 rounded-lg px-4 "
+                        >
+                          <Input
+                            className="w-[70%]"
+                            value={newTodoTitle}
+                            onChange={(e) => setNewTodoTitle(e.target.value)}
+                            type="text"
+                            placeholder="Enter new todo title..."
+                          />
+                          <button className="text-primary">
+                            <Vote size={20} className="text-primary" />
+                          </button>
+                        </form>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : (
+              <p>No todos yet...</p>
+            )}
+            <form
+              onSubmit={(e) => handleCreateTodo(e, task._id)}
+              className="bg-secondary flex flex-col gap-2 p-3"
+            >
+              <h1 className="text-center font-bold">Add todo</h1>
+              <Input
+                type="text"
+                value={createNewTodoTitle}
+                onChange={(e) => setCreateNewTodoTitle(e.target.value)}
+                placeholder="Enter new todo..."
+              />
+              <Button>Add</Button>
+            </form>
+          </div>
+        </div>
+        <div className="flex gap-4 absolute top-0 right-4">
           <Button
             onClick={handleDelete}
             variant="outlet"
@@ -132,6 +308,7 @@ function TaskDeatailsPage() {
           <Button
             variant="outlet"
             className="text-destructive border border-primary hover:bg-primary/90"
+            onClick={() => setEditTaskInputs(!editTaskInputs)}
           >
             <Pencil className="text-primary" />
           </Button>
@@ -165,4 +342,4 @@ function TaskDeatailsPage() {
   );
 }
 
-export default TaskDeatailsPage;
+export default TaskDetailsPage;
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -2,7 +2,7 @@ import TasksList from "@/components/react-omponenets/TasksList";
 import { useAuth } from "@/contexts/AuthContext";
 import { useLoggedInUserTasks } from "@/contexts/loggedInUserTasksContext";
 import api from "@/services/api.service";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 
@@ -10,6 +10,9 @@ function TasksPage() {
   const { loggedInUser } = useAuth();
   const { loggedInUserTasks, setLoggedInUserTasks } = useLoggedInUserTasks();
   const navigate = useNavigate();
+
+  const pinnedTasks = loggedInUserTasks.filter((task) => task.isPinned);
+  const otherTasks = loggedInUserTasks.filter((task) => !task.isPinned);
 
   useEffect(() => {
     async function fetchLoggedInUserTasks() {
@@ -34,13 +37,29 @@ function TasksPage() {
       <h1 className="text-5xl font-extrabold p-10">
         {loggedInUser && `${loggedInUser.firstName}'s Tasks`}
       </h1>
-      <div className="flex justify-center items-center ">
-        {loggedInUserTasks.length > 0 ? (
-          <TasksList loggedInUserTasks={loggedInUserTasks} />
-        ) : (
-          <p>No Tasks yet...</p>
+      <div className="flex flex-col gap-6 ">
+        {pinnedTasks.length > 0 && (
+          <>
+            <div className="shadow-2xl p-8">
+              <h2 className="text-center text-3xl font-bold mb-4">
+                Pinned Tasks
+              </h2>
+              <TasksList loggedInUserTasks={pinnedTasks} />
+            </div>
+          </>
+        )}
+        {otherTasks.length > 0 && (
+          <>
+            <div className="shadow-2xl p-8">
+              <h2 className="text-center text-3xl font-bold mb-4">Tasks</h2>
+              <TasksList loggedInUserTasks={otherTasks} />
+            </div>
+          </>
         )}
       </div>
+      {pinnedTasks.length === 0 && otherTasks.length === 0 && (
+        <p>No Tasks yet...</p>
+      )}
       <Outlet />
     </div>
   );
